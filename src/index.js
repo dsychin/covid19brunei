f2a6e392949cac3fb0672d716897682b7107f762
Desmond Chin <chin.sy.des@gmail.com>
2020-03-13 22:26:02 +0800

    add data

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import './scss/style.scss';
 import data from './data.json';
 
 // update statistics
+document.getElementById('last-update').innerText = data.lastUpdated;
+
 document.getElementById('case-number').innerText = data.statistics.cases.total;
 document.getElementById('death-number').innerText = data.statistics.deaths.total;
 document.getElementById('recover-number').innerText = data.statistics.recovered.total;
 
 let ctx = document.getElementById('chart');
 let chart = new Chart(ctx, {
-    type: 'bar',
-    data: [1, 3, 11, 25]
+    type: 'line',
+    data: {
+        labels: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5'
+        ],
+        datasets: [{
+            label: 'Number of Cases',
+            data: [1, 6, 11, 25, 37]
+        }]
+    } 
 });
