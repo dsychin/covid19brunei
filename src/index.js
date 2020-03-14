9b13f26929d9f92548470b711dd81e04ad98745c
Desmond Chin <chin.sy.des@gmail.com>
2020-03-14 08:14:50 +0800

    change data points to use date

--- src/index.js
+++ src/index.js
@@ -13,19 +13,32 @@ let ctx = document.getElementById('chart');
 let chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: [
-            '9 March',
-            '10 March',
-            '11 March',
-            '12 March',
-            '13 March'
-        ],
         datasets: [{
             label: 'Number of Cases',
             fill: false,
             borderColor: '#dc3545',
             backgroundColor: '#dc3545',
-            data: [1, 6, 11, 25, 37]
+            data: [
+                {
+                    t: '2020-03-09',
+                    y: 1
+                },
+                {
+                    t: '2020-03-10',
+                    y: 6
+                },
+                {
+                    t: '2020-03-11',
+                    y: 11
+                },
+                {
+                    t: '2020-03-12',
+                    y: 25
+                },
+                {
+                    t: '2020-03-13',
+                    y: 37
+                }]
         }]
     },
     options: {
@@ -34,6 +47,19 @@ let chart = new Chart(ctx, {
         title: {
             display: true,
             text: 'Brunei COVID-19 Case Trends'
+        },
+        scales: {
+            xAxes: [{
+                type: 'time',
+                distribution: 'linear',
+                time: {
+                    unit: 'day',
+                    unitStepSize: 1,
+                    displayFormats: {
+                        day: 'DD MMM'
+                    }
+                }
+            }]
         }
     }
 });
