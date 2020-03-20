176c48e4cb6c42004b22d75611b02fe2bf8cbe64
Desmond Chin <chin.sy.des@gmail.com>
2020-03-20 13:56:12 +0800

    change table to use material-table

--- src/components/Cases/index.js
+++ src/components/Cases/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import{ Table, TableRow, TableCell, TableContainer, Paper, TableHead, TableBody } from '@material-ui/core';
+import React, { forwardRef } from 'react';
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 import data from '../../data/data';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
 const Cases = () => {
     return (
         <div>
-            Hello world
             <BruneiCases />
         </div>
     )
@@ -17,46 +51,17 @@ export default Cases;
 const BruneiCases = () => {
     return (
         <div>
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>
-                                Case
-                            </TableCell>
-                            <TableCell>
-                                Age
-                            </TableCell>
-                            <TableCell>
-                                Gender
-                            </TableCell>
-                            <TableCell>
-                                Description
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.cases.map(value => {
-                            return (
-                                <TableRow>
-                                    <TableCell>
-                                        {value.caseNumber}
-                                    </TableCell>
-                                    <TableCell>
-                                        {value.age}
-                                    </TableCell>
-                                    <TableCell>
-                                        {value.gender}
-                                    </TableCell>
-                                    <TableCell>
-                                        {value.description}
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <MaterialTable
+                icons={ tableIcons }
+                title="Brunei Cases"
+                columns={[
+                    { title: 'Case', field: 'caseNumber', type:'numeric', width: 100 },
+                    { title: 'Age', field: 'age', cellStyle: { width: 'auto' }, headerStyle: { width: 'auto' } },
+                    { title: 'Gender', field: 'gender', cellStyle: { width: 'auto' }, headerStyle: { width: 'auto' } },
+                    { title: 'Description', field: 'description', cellStyle: { width: 'auto' }, headerStyle: { width: 'auto' } },
+                ]}
+                data={ data.cases }
+            />
         </div>
     )
 }
