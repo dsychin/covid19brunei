2fae5e2e5be2894d8d62433e5c09bd3943d98ec3
Desmond Chin <chin.sy.des@gmail.com>
2020-03-30 17:58:51 +0800

    update data to use api only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import preval from "preval.macro";
+import Axios from "axios";
 
 import Stats from "./components/Stats";
 import Trends from "./components/Trends";
@@ -11,23 +12,35 @@ import Cases from "./components/Cases";
 
 const build_date = preval`module.exports = new Date()`;
 
-const Title = ({ date }) => (
-  <>
-    <h1>Brunei COVID-19 Tracker</h1>
+const Title = ({ date }) => {
+  const [lastUpdated, setLastUpdated] = useState('');
 
-    <div className="last-updated">
-      Last Updated:{" "}
-      {new Date(date).toLocaleString("en-SG", {
+  useEffect(() => {
+    const getLastUpdated = async () => {
+      const response = await Axios.get('https://covid19.mathdro.id/api/countries/BN');
+      setLastUpdated(new Date(response.data.lastUpdate).toLocaleString("en-SG", {
         timeZone: "Asia/Singapore",
         day: "numeric",
         month: "long",
         year: "numeric",
         hour: "numeric",
         minute: "numeric"
-      })}
-    </div>
-  </>
-);
+      }));
+    }
+    getLastUpdated();
+  },[]);
+
+  return (
+    <>
+      <h1>Brunei COVID-19 Tracker</h1>
+  
+      <div className="last-updated">
+        Last Updated:{" "}
+        {lastUpdated}
+      </div>
+    </>
+  );
+}
 
 const App = () => {
   return (
